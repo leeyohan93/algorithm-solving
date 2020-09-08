4fde36ec1b633bb6b1342caefe8d1157a9228c1a
yohan <good_1411@naver.com>
2020-09-09 01:16:27 +0900

    refactor: 프로그래머스.해쉬.베스트앨범 리뷰

--- src/test/java/programmers/hash/bestalbum/Solution.java
+++ src/test/java/programmers/hash/bestalbum/Solution.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * 스트림의 활용과 map() vs flatMap()의 차이점을 확실히 알 수 있도록 한다.
+ * sort 의 활용도를 생각하고 Comparable 구현을 통한 방법은 코드를 많이 깔끔하게 만든다.
+ */
 public class Solution {
 
     @Test
