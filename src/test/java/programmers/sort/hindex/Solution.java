88a4c5bd48f8f8317e153b5f8cc6a73b240e2e18
yohan <good_1411@naver.com>
2020-09-26 18:45:20 +0900

    feat: 프로그래머스.sort.hindex other solution 추가

--- src/test/java/programmers/sort/hindex/Solution.java
+++ src/test/java/programmers/sort/hindex/Solution.java
@@ -2,6 +2,8 @@ package programmers.sort.hindex;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Solution {
@@ -14,5 +16,19 @@ public class Solution {
         int hindex = papers.getHindex();
 
         assertThat(hindex).isEqualTo(5);
+    }
+
+    @Test
+    void otherSolve() {
+        int[] citations = {12, 11, 10, 9, 8, 1};
+
+        int answer = 0;
+        Arrays.sort(citations);
+        for(int i=0; i<citations.length; i++) {
+            int smaller = Math.min(citations[i], citations.length - i);
+            answer = Math.max(answer, smaller);
+        }
+
+        assertThat(answer).isEqualTo(5);
     }
 }
